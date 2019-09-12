312e9208663ade4f1beb6373400d526d88b7b01d
AJAY-MAC <letv4545@gmail.com>
2019-09-12 11:39:55 -0400

    Test Case 1 (isHealthy()) completed.

--- tests/SnakeTest.java
+++ tests/SnakeTest.java
@@ -6,8 +6,17 @@ import org.junit.Test;
 
 public class SnakeTest {
 
+	Snake snake1;
+	Snake snake2;
+	
 	@Before
 	public void setUp() throws Exception {
+		
+		// Make a new snake objects that all test cases can use
+		snake1 = new Snake("Peter",10,"coffee");
+		snake2 = new Snake("Takis",80,"vegetables");
+		
+		
 	}
 
 	@After
@@ -15,8 +24,18 @@ public class SnakeTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void isHealthyTest() {
+		//check if snake1 is healthy
+		assertTrue(snake1.isHealthy());
+		
+		//check if snake2 is healthy
+		assertTrue(snake2.isHealthy());
+		
+		//check if snake1 is unhealthy
+		assertFalse(snake1.isHealthy());
+		
+		//check if snake2 is unhealthy
+		assertFalse(snake2.isHealthy());
 	}
 
 	
