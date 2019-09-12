dac5a9f7ef7f5f9d63b15bcd4c2a32acb08929d6
AJAY-MAC <letv4545@gmail.com>
2019-09-12 11:46:09 -0400

    BlazeDemo test file with selenium setup and URL to be visited.

--- tests/BlazeDemoTest.java
+++ tests/BlazeDemoTest.java
@@ -3,20 +3,41 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDemoTest {
+	//Location of Chromedriver file
+	final String CHROMEDRIVER_LOCATION = "/Users/AJAY-MAC/chromedriver"; 
+	
+	// Website to be tested
+	final String URL_TO_TEST = "http://www.blazedemo.com";
+	
+	//Global driver variables
+	WebDriver driver;
 
 	@Before
 	public void setUp() throws Exception {
+		
+	// Selenium setup
+	System.setProperty("webdriver.chrome.driver",CHROMEDRIVER_LOCATION);
+	driver = new ChromeDriver();
+					
+	//Go to Website
+	driver.get(URL_TO_TEST);
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		//After you run the test case, close the browser
+		Thread.sleep(10000);
+		driver.close();
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
