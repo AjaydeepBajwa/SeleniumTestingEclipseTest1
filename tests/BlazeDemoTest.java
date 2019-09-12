74b185d5354767a46c02ef189f34aba5bd2363e5
AJAY-MAC <letv4545@gmail.com>
2019-09-12 12:14:50 -0400

    testing the number of departure cities

--- tests/BlazeDemoTest.java
+++ tests/BlazeDemoTest.java
@@ -1,9 +1,13 @@
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BlazeDemoTest {
@@ -25,19 +29,29 @@ public class BlazeDemoTest {
 					
 	//Go to Website
 	driver.get(URL_TO_TEST);
+	
+	
 		
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		//After you run the test case, close the browser
-		Thread.sleep(10000);
+		Thread.sleep(4000);
 		driver.close();
 	}
 
 	@Test
-	public void test() {
+	public void departureCityCountTest() {
+		// Get the total cities from departure and destination box.
+		List<WebElement> CitySelBox = driver.findElements(By.cssSelector("form select"));
 		
+		//select the departure cities
+		WebElement  depCityList = CitySelBox.get(0);
+		//Get the list of departure cities
+		List<WebElement> Listt = depCityList.findElements(By.cssSelector("option"));
+		System.out.println("No. of Departure cities" +Listt.size());
+		assertEquals(7,Listt.size());		
 	}
 
 }
